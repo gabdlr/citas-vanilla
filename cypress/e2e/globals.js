bfb6f5a803b77ca735a5071c55d9754869215bc3
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-11-05 00:06:26 -0300

    test(e2e): set global var to reference dom element

---
+++ cypress/e2e/globals.js
@@ -0,0 +1,28 @@
+export const globals = {
+  app: {
+    header: "[data-testId='appHeader']",
+  },
+  formContainer: {
+    formContainer: "[data-testId='pacientFormContainer']",
+    subtitle: "[data-testId='formContainerSubtitle']",
+  },
+  form: {
+    form: "[data-testId='pacientForm']",
+    pacientNameInput: "[data-testId='pacientNameInput']",
+    pacientOwnerInput: "[data-testId='pacientOwnerNameInput']",
+    pacientEmailInput: "[data-testId='pacientEmailInput']",
+    signUpDateInput: "[data-testId='signUpDateInput']",
+    pacientSymptomsInput: "[data-testId='pacientSymptomsInput']",
+    pacientIdInput: "[data-testId='pacientIdInput']",
+    submitButtonId: "[data-testId='submitButtonId']",
+  },
+  pacientList: {
+    pacientList: "[data-testId='pacientList']",
+    pacientListSubtitle: "[data-testId='pacientListSubtitle']",
+  },
+  pacientCard: {
+    fieldParagraph: "[data-testId='pacientCardField']",
+    editButton: "[data-testId='pacientCardEditButton']",
+    deleteButton: "[data-testId='pacientCardDeleteButton']",
+  },
+};
